90963c406f1abdb6a28f47f84efac28fa2e1de41
Alexandra <alex_gorskaya1@mail.ru>
2022-03-02 09:55:58 +0300

    mater updated

--- src/containers/FilterArea/FilterArea.js
+++ src/containers/FilterArea/FilterArea.js
@@ -12,7 +12,6 @@ import {
 	Typography,
 } from '@mui/material';
 import LocalSwitch from '../../providers/Locales/LocalSwitch';
-import ThemeSwitch from '../../providers/Theme/ThemeSwitch';
 import FemaleIcon from '@mui/icons-material/Female';
 import MaleIcon from '@mui/icons-material/Male';
 import { useTranslation } from 'react-i18next';
@@ -131,7 +130,6 @@ const FilterArea = ({
 				</FormControl>
 				<Stack column>
 					<LocalSwitch />
-					<ThemeSwitch />
 				</Stack>
 			</Stack>
 
